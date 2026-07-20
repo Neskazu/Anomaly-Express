9917d61901599803809e95e1d826be4eb1e79cdc
Neskazu <47606963+Neskazu@users.noreply.github.com>
2026-07-20 17:06:38 +0600

    Adjusted parallax

--- Assets/5 - Scripts/Runtime/UI/UIJuice/UIParallaxLooper.cs
+++ Assets/5 - Scripts/Runtime/UI/UIJuice/UIParallaxLooper.cs
@@ -3,51 +3,104 @@ using UnityEngine;
 [System.Serializable]
 public class ParallaxUILevel
 {
-    public RectTransform a;
-    public RectTransform b;
+    public RectTransform[] elements;
     public float speed = 50f;
 }
 
 public class UIParallaxLooper : MonoBehaviour
 {
-    public ParallaxUILevel far;
-    public ParallaxUILevel mid;
-    public ParallaxUILevel near;
+    [SerializeField] private ParallaxUILevel far;
+    [SerializeField] private ParallaxUILevel mid;
+    [SerializeField] private ParallaxUILevel near;
+
+    private RectTransform _canvasRect;
+
+    private void Start()
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas != null)
+        {
+            _canvasRect = canvas.rootCanvas.transform as RectTransform;
+        }
+        else
+        {
+            foreach (var el in far.elements)
+            {
+                if (el != null)
+                {
+                    _canvasRect = el.GetComponentInParent<Canvas>()?.rootCanvas.transform as RectTransform;
+                    break;
+                }
+            }
+        }
+    }
 
     private void Update()
     {
+        if (_canvasRect == null) return;
+
         MoveLevel(far);
         MoveLevel(mid);
         MoveLevel(near);
     }
 
-    void MoveLevel(ParallaxUILevel level)
+    private void MoveLevel(ParallaxUILevel level)
     {
-        if (level == null || level.a == null || level.b == null)
+        if (level == null || level.elements == null || level.elements.Length < 2)
             return;
 
         float delta = level.speed * Time.deltaTime;
 
-        level.a.anchoredPosition += Vector2.left * delta;
-        level.b.anchoredPosition += Vector2.left * delta;
-
-        float widthA = level.a.rect.width;
-        float widthB = level.b.rect.width;
-
-        if (level.a.anchoredPosition.x <= -widthA)
+        foreach (var element in level.elements)
         {
-            level.a.anchoredPosition = new Vector2(
-                level.b.anchoredPosition.x + widthB,
-                level.a.anchoredPosition.y
-            );
+            if (element == null) continue;
+            element.anchoredPosition += Vector2.left * delta;
         }
 
-        if (level.b.anchoredPosition.x <= -widthB)
+        Vector3[] currentCorners = new Vector3[4];
+        Vector3[] rightMostCorners = new Vector3[4];
+
+        RectTransform rightMost = null;
+        float maxRightX = float.MinValue;
+
+        foreach (var element in level.elements)
         {
-            level.b.anchoredPosition = new Vector2(
-                level.a.anchoredPosition.x + widthA,
-                level.b.anchoredPosition.y
-            );
+            if (element == null) continue;
+            element.GetWorldCorners(currentCorners);
+
+            if (currentCorners[2].x > maxRightX)
+            {
+                maxRightX = currentCorners[2].x;
+                rightMost = element;
+                System.Array.Copy(currentCorners, rightMostCorners, 4);
+            }
+        }
+
+        Vector3[] canvasCorners = new Vector3[4];
+        _canvasRect.GetWorldCorners(canvasCorners);
+        float screenLeftEdgeWorld = canvasCorners[0].x; 
+
+        foreach (var element in level.elements)
+        {
+            if (element == null) continue;
+            element.GetWorldCorners(currentCorners);
+
+            if (currentCorners[2].x <= screenLeftEdgeWorld)
+            {
+                RectTransform parentRect = element.parent as RectTransform;
+                if (parentRect == null) continue;
+
+                Vector3 rightMostRightEdgeWorld = rightMostCorners[2];
+                Vector3 localPosOfRightEdge = parentRect.InverseTransformPoint(rightMostRightEdgeWorld);
+
+                float pivotOffset = element.rect.width * element.pivot.x;
+                float newLocalX = localPosOfRightEdge.x + pivotOffset;
+
+                element.anchoredPosition = new Vector2(newLocalX, element.anchoredPosition.y);
+
+                rightMost = element;
+                element.GetWorldCorners(rightMostCorners);
+            }
         }
     }
 }
