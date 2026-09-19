69f93bfbe89c982c55c8ed15f969075d47057d6c
Fuwii <fuwii.workspace@gmail.com>
2026-09-19 13:55:07 +0300

    fix settings sync

--- Assets/5 - Scripts/Runtime/Settings/GraphicsSettingsSave.cs
+++ Assets/5 - Scripts/Runtime/Settings/GraphicsSettingsSave.cs
@@ -2,12 +2,31 @@ using System;
 using R3;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
 
 namespace Nac
 {
     [Serializable]
     public class GraphicsSettingsSave : ISerializationCallbackReceiver
     {
+        private static List<Resolution> resolutions = Screen.resolutions
+            .GroupBy(res => new { res.width, res.height })
+            .Select(group => group.Last())
+            .ToList();
+
+        private static List<string> antiAliasingNames = new()
+        {
+            "None",
+            "MSAAx2",
+            "MSAAx4",
+            "MSAAx8",
+            "FXAA",
+            "SMAA",
+            "TAA",
+        };
+
         [SerializeField] private AntialiasingMode antialiasingMode;
         [SerializeField] private MsaaQuality msaaQuality;
         [SerializeField] private float brightness;
@@ -15,6 +34,10 @@ namespace Nac
         [NonSerialized] private ReactiveProperty<AntialiasingMode> reactiveAntialiasing = new(AntialiasingMode.None);
         [NonSerialized] private ReactiveProperty<MsaaQuality> reactiveMsaaQuality = new(MsaaQuality.Disabled);
         [NonSerialized] private ReactiveProperty<float> reactiveBrightness = new(0.0f);
+
+        [NonSerialized] private readonly Subject<Unit> onSettingsChanged = new();
+
+        public R3.Observable<Unit> OnSettingsChanged => onSettingsChanged;
 
         public ReadOnlyReactiveProperty<AntialiasingMode> Antialiasing => reactiveAntialiasing;
 
@@ -25,20 +48,159 @@ namespace Nac
 
         public ReadOnlyReactiveProperty<float> Brightness => reactiveBrightness;
 
-        public void SetAntialiasing(AntialiasingMode antialiasing)
+        public int GetAntiAliasingIndex()
         {
-            reactiveAntialiasing.Value = antialiasing;
+            switch (Antialiasing.CurrentValue)
+            {
+                case AntialiasingMode.FastApproximateAntialiasing:
+                    return 4;
+                case AntialiasingMode.SubpixelMorphologicalAntiAliasing:
+                    return 5;
+                case AntialiasingMode.TemporalAntiAliasing:
+                    return 6;
+                case AntialiasingMode.None:
+                default:
+                    switch (Msaa.CurrentValue)
+                    {
+                        case MsaaQuality._2x:
+                            return 1;
+                        case MsaaQuality._4x:
+                            return 2;
+                        case MsaaQuality._8x:
+                            return 3;
+                        case MsaaQuality.Disabled:
+                        default:
+                            return 0;
+                    }
+            }
         }
 
-        public void SetMsaaQuality(MsaaQuality quality)
+        public int GetResolutionIndex()
         {
-            reactiveMsaaQuality.Value = quality;
+            return resolutions.FindIndex(r =>
+                r.height == Screen.height &&
+                r.width == Screen.width);
+        }
+
+        public bool GetFullScreen()
+        {
+            return Screen.fullScreenMode == FullScreenMode.FullScreenWindow;
+        }
+
+        public bool GetVsync()
+        {
+            return QualitySettings.vSyncCount == 1;
+        }
+
+        public float GetShadow()
+        {
+            return PlayerPrefs.GetFloat("Shadow", 0);
+        }
+
+        public float GetBrightness()
+        {
+            return Brightness.CurrentValue;
+        }
+
+        public void SetResolution(int index)
+        {
+            if (index >= resolutions.Count)
+            {
+                return;
+            }
+
+            var resolution = resolutions[index];
+
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode);
+
+            onSettingsChanged.OnNext(Unit.Default);
+        }
+
+        public void SetFullScreen(bool isFullScreen)
+        {
+            Screen.SetResolution(Screen.width, Screen.height, isFullScreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed);
+            Screen.fullScreen = isFullScreen;
+
+            onSettingsChanged.OnNext(Unit.Default);
+        }
+
+        public void SetAntialiasing(int index)
+        {
+            switch (index)
+            {
+                default:
+                    reactiveAntialiasing.Value = AntialiasingMode.None;
+                    reactiveMsaaQuality.Value = MsaaQuality.Disabled;
+                    break;
+                case 1:
+                    reactiveAntialiasing.Value = AntialiasingMode.None;
+                    reactiveMsaaQuality.Value = MsaaQuality._2x;
+                    break;
+                case 2:
+                    reactiveAntialiasing.Value = AntialiasingMode.None;
+                    reactiveMsaaQuality.Value = MsaaQuality._4x;
+                    break;
+                case 3:
+                    reactiveAntialiasing.Value = AntialiasingMode.None;
+                    reactiveMsaaQuality.Value = MsaaQuality._8x;
+                    break;
+                case 4:
+                    reactiveAntialiasing.Value = AntialiasingMode.FastApproximateAntialiasing;
+                    reactiveMsaaQuality.Value = MsaaQuality.Disabled;
+                    break;
+                case 5:
+                    reactiveAntialiasing.Value = AntialiasingMode.SubpixelMorphologicalAntiAliasing;
+                    reactiveMsaaQuality.Value = MsaaQuality.Disabled;
+                    break;
+                case 6:
+                    reactiveAntialiasing.Value = AntialiasingMode.TemporalAntiAliasing;
+                    reactiveMsaaQuality.Value = MsaaQuality.Disabled;
+                    break;
+            }
+
+            onSettingsChanged.OnNext(Unit.Default);
         }
 
         public void SetBrightness(float val)
         {
             reactiveBrightness.Value = Mathf.Clamp(val, -1f, 1f);
+
+            onSettingsChanged.OnNext(Unit.Default);
         }
+
+        public void SetShadow(float value)
+        {
+            var target = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
+
+            if (target != null)
+            {
+                target.shadowDistance = value;
+            }
+
+            PlayerPrefs.SetFloat("Shadow", value);
+
+            onSettingsChanged.OnNext(Unit.Default);
+        }
+
+        public void SetVsync(bool isOn)
+        {
+            QualitySettings.vSyncCount = isOn ? 1 : 0;
+            PlayerPrefs.SetInt("VSync", isOn ? 1 : 0);
+
+            onSettingsChanged.OnNext(Unit.Default);
+        }
+
+        public List<Resolution> GetAllResolutions()
+        {
+            return resolutions;
+        }
+
+        public List<string> GetAllAntiAliasing()
+        {
+            return antiAliasingNames;
+        }
+
+        #region ISerializationCallbackReceiver
 
         public void OnBeforeSerialize()
         {
@@ -53,5 +215,7 @@ namespace Nac
             reactiveMsaaQuality.Value = msaaQuality;
             reactiveBrightness.Value = brightness;
         }
+
+        #endregion
     }
 }
--- Assets/5 - Scripts/Runtime/UI/Settings/UiGraphicsSettings.cs
+++ Assets/5 - Scripts/Runtime/UI/Settings/UiGraphicsSettings.cs
@@ -1,21 +1,19 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Nac;
+using Nac.Extensions;
 using R3;
 using SaveSystem;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 namespace UI.Settings
 {
     public class UiGraphicsSettings : MonoBehaviour
     {
-        [Header("References")]
+        private static GraphicsSettingsSave Graphics => SaveManager.Save.GraphicsSettings;
+
         [SerializeField] private TMP_Dropdown resolution;
         [SerializeField] private Toggle fullScreen;
         [SerializeField] private Toggle vsync;
@@ -23,37 +21,23 @@ namespace UI.Settings
         [SerializeField] private Slider shadow;
         [SerializeField] private TMP_Dropdown antiAliasing;
 
-        private UniversalRenderPipelineAsset target;
-        private List<Resolution> resolutions;
-
-        private GraphicsSettingsSave Graphics => SaveManager.Save.GraphicsSettings;
-
         private void Awake()
         {
-            target = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
-
-            resolutions = Screen.resolutions
-                .DistinctBy(r => new { r.width, r.height })
-                .ToList();
-
-            resolution.options = resolutions
+            resolution.options = Graphics
+                .GetAllResolutions()
                 .Select(r => new TMP_Dropdown.OptionData($"{r.width}x{r.height}"))
                 .ToList();
 
-            antiAliasing.options = new List<TMP_Dropdown.OptionData>()
-            {
-                new("None"),
-                new("MSAAx2"),
-                new("MSAAx4"),
-                new("MSAAx8"),
-                new("FXAA"),
-                new("SMAA"),
-                new("TAA"),
-            };
+            antiAliasing.options = Graphics
+                .GetAllAntiAliasing()
+                .Select(aa => new TMP_Dropdown.OptionData($"{aa}"))
+                .ToList();
         }
 
         private void Start()
         {
+            Sync();
+
             resolution.OnValueChangedAsObservable()
                 .Subscribe(ApplyResolution)
                 .AddTo(this);
@@ -77,127 +61,58 @@ namespace UI.Settings
             antiAliasing.OnValueChangedAsObservable()
                 .Subscribe(ApplyAntiAliasing)
                 .AddTo(this);
+
+            Graphics.OnSettingsChanged
+                .Delay(TimeSpan.FromMilliseconds(500f))
+                .Subscribe(Sync)
+                .AddTo(this);
         }
 
-        private void OnEnable()
+        private void Sync()
         {
-            var currentResIndex = resolutions.FindIndex(r =>
-                r.width == Screen.width &&
-                r.height == Screen.height);
+            antiAliasing.SetValueWithoutNotify(Graphics.GetAntiAliasingIndex());
+            resolution.SetValueWithoutNotify(Graphics.GetResolutionIndex());
 
-            resolution.value = currentResIndex != -1 ? currentResIndex : resolutions.Count - 1;
+            fullScreen.SetIsOnWithoutNotify(Graphics.GetFullScreen());
+            vsync.SetIsOnWithoutNotify(Graphics.GetVsync());
+
+            brightness.SetValueWithoutNotify(Graphics.GetBrightness());
+            shadow.SetValueWithoutNotify(Graphics.GetShadow());
+
             resolution.RefreshShownValue();
-
-            brightness.value = Graphics.Brightness.CurrentValue;
-
-            switch (Graphics.Antialiasing.CurrentValue)
-            {
-                case AntialiasingMode.FastApproximateAntialiasing:
-                    antiAliasing.value = 4;
-                    break;
-                case AntialiasingMode.SubpixelMorphologicalAntiAliasing:
-                    antiAliasing.value = 5;
-                    break;
-                case AntialiasingMode.TemporalAntiAliasing:
-                    antiAliasing.value = 6;
-                    break;
-                case AntialiasingMode.None:
-                default:
-                    switch (Graphics.Msaa.CurrentValue)
-                    {
-                        case MsaaQuality._2x:
-                            antiAliasing.value = 1;
-                            break;
-                        case MsaaQuality._4x:
-                            antiAliasing.value = 2;
-                            break;
-                        case MsaaQuality._8x:
-                            antiAliasing.value = 3;
-                            break;
-                        case MsaaQuality.Disabled:
-                        default:
-                            antiAliasing.value = 0;
-                            break;
-                    }
-
-                    break;
-            }
-
             antiAliasing.RefreshShownValue();
-            fullScreen.isOn = Screen.fullScreen;
+
+            SaveManager.SaveGame();
         }
 
         private void ApplyResolution(int index)
         {
-            if (index >= 0 && index < resolutions.Count)
-            {
-                var res = resolutions[index];
-                Screen.SetResolution(res.width, res.height, Screen.fullScreen);
-            }
+            Graphics.SetResolution(index);
         }
 
         private void ApplyFullScreen(bool isOn)
         {
-            Screen.fullScreen = isOn;
+            Graphics.SetFullScreen(isOn);
         }
 
         private void ApplyVSync(bool isOn)
         {
-            QualitySettings.vSyncCount = isOn ? 1 : 0;
-            PlayerPrefs.SetInt("VSync", isOn ? 1 : 0);
+            Graphics.SetVsync(isOn);
         }
 
         private void ApplyBrightness(float val)
         {
             Graphics.SetBrightness(val);
-            SaveManager.SaveGame();
         }
 
         private void ApplyShadow(float val)
         {
-            if (target != null)
-            {
-                target.shadowDistance = val;
-            }
-
-            PlayerPrefs.SetFloat("Shadow", val);
+            Graphics.SetShadow(val);
         }
 
         private void ApplyAntiAliasing(int index)
         {
-            switch (index)
-            {
-                default:
-                    Graphics.SetAntialiasing(AntialiasingMode.None);
-                    Graphics.SetMsaaQuality(MsaaQuality.Disabled);
-                    break;
-                case 1:
-                    Graphics.SetAntialiasing(AntialiasingMode.None);
-                    Graphics.SetMsaaQuality(MsaaQuality._2x);
-                    break;
-                case 2:
-                    Graphics.SetAntialiasing(AntialiasingMode.None);
-                    Graphics.SetMsaaQuality(MsaaQuality._4x);
-                    break;
-                case 3:
-                    Graphics.SetAntialiasing(AntialiasingMode.None);
-                    Graphics.SetMsaaQuality(MsaaQuality._8x);
-                    break;
-                case 4:
-                    Graphics.SetAntialiasing(AntialiasingMode.FastApproximateAntialiasing);
-                    Graphics.SetMsaaQuality(MsaaQuality.Disabled);
-                    break;
-                case 5:
-                    Graphics.SetAntialiasing(AntialiasingMode.SubpixelMorphologicalAntiAliasing);
-                    Graphics.SetMsaaQuality(MsaaQuality.Disabled);
-                    break;
-                case 6:
-                    Graphics.SetAntialiasing(AntialiasingMode.TemporalAntiAliasing);
-                    Graphics.SetMsaaQuality(MsaaQuality.Disabled);
-                    break;
-            }
-
-            SaveManager.SaveGame();
+            Graphics.SetAntialiasing(index);
         }
     }
 }
